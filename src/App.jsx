2913c9525610c22bf8eb4e5cde8bc500d89c91e8
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-20 01:38:44 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import {useState} from "react"
 import { Form } from "./components/Form";
-
+import { nanoid } from "nanoid";
 const App = () => {
-const [items, setItems] = useState([])  
+
+ // so in this case, we didnt import an obj that had KVP's already
+// we just create the state variable name that corresponed to an an empty list
+// what we want to do now is create an obj literal that will have KVP's that can be updated or removed 
+// we can then assign the obj to a variable and display the list 
+//**! [items] Array =[]  */
+  const [items, setItems] = useState([])  
+
+
+  //!Creating objl list of kvp's
+ //*! {addItem} Function =    */
+ const addItem =(newItemName) => {
+  const newItem = {
+    id:nanoid(),
+    name:newItemName,
+    completed:false,
+    }
+  
+    setItems([...items,newItem])//!updates state value
+  console.log(items);//! logs updated state value
+}
+
 return (
   <section className="section-center">
-    <Form />
+    <Form addItem={addItem}/>
   </section>
   )
 };
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,23 @@
 import {useState} from 'react'
 
-export const Form = () => {
+export const Form = ({addItem}) => {
 const [newItemName, setNewItemName] = useState('')  
 const handleSubmit = (e) => {
   e.preventDefault();
+  
+//we want to create a conditional to make sure 
+//that there is an item to add before we invoke the 
+//addItem() function
+//! We can also set the input-value back to an empty string 
+
+  if(!newItemName) return;
+  addItem(newItemName);
+  setNewItemName('');
+ 
+
 }
     return (
-        <form className="">
+        <form onSubmit={handleSubmit}>
             <h4>Grocery Buds</h4>
             <div className='form-control'>
                 <input 
@@ -16,7 +27,7 @@ const handleSubmit = (e) => {
                     value={newItemName} placeholder='Add item' 
                     onChange={(e) => setNewItemName(e.target.value)}
                 />
-                <button type="submit" className="btn" onSubmit={handleSubmit}>
+                <button type="submit" className="btn" >
                     add Item 
                 </button>
             </div>
